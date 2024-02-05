699a9234e6b9392104f8739f297e9fa319bdc206
Lukasz Kalnik <lukasz.kalnik@rewe-digital.com>
2024-02-05 13:47:47 +0100

    CouponRepository returns Either

--- app/src/main/java/com/example/functionalerrorhandlingslides/CouponRepository.kt
+++ app/src/main/java/com/example/functionalerrorhandlingslides/CouponRepository.kt
@@ -1,8 +1,12 @@
 package com.example.functionalerrorhandlingslides
 
+import arrow.core.Either
+import arrow.core.right
+import arrow.retrofit.adapter.either.networkhandling.CallError
+
 class CouponRepository {
 
-    fun getCouponId(productId: String): String {
-        return "couponId"
+    fun getCouponId(productId: String): Either<CallError, String> {
+        return "couponId".right()
     }
 }
